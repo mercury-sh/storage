a72b9f0b21f42849ce37dcde1dcc9efe27c1129a
Bruno Sales <me@baliestri.dev>
2024-11-12 10:24:13 -0300

    add comparison and equality implementation

--- source/Mercury.PowerShell.Storage/BaseEntity.cs
+++ source/Mercury.PowerShell.Storage/BaseEntity.cs
@@ -1,6 +1,7 @@
 // Copyright (c) Bruno Sales <me@baliestri.dev>. Licensed under the MIT License.
 // See the LICENSE file in the repository root for full license text.
 
+using System.Diagnostics;
 using SQLite;
 
 namespace Mercury.PowerShell.Storage;
@@ -9,7 +10,8 @@ namespace Mercury.PowerShell.Storage;
 ///   Represents the base entity of the system.
 /// </summary>
 [Serializable]
-public class BaseEntity {
+[DebuggerDisplay("{ToString(),nq}")]
+public abstract class BaseEntity : IComparable<BaseEntity>, IEquatable<BaseEntity> {
   // Since the type Ulid is not serializable in SQLite-net (and there is no way to extend it), we store it as a string with a backing field.
   // Long or Int types are not used as PK because we would need to save the entity in the database to get the ID.
   // Guid is not used because it's the version 4 of the UUID, and I would like to use the version 7.
@@ -23,10 +25,30 @@ public class BaseEntity {
   [MaxLength(26)]
   public string Id { get => _pk.ToString(); init => _pk = Ulid.Parse(value); }
 
+  /// <inheritdoc />
+  public int CompareTo(BaseEntity? other)
+    => other is null ? 1 : _pk.CompareTo(other._pk);
+
+  /// <inheritdoc />
+  public bool Equals(BaseEntity? other)
+    => other is not null && _pk.Equals(other._pk);
+
   /// <summary>
   ///   Gets the primary key of the entity as an <see cref="Ulid" />.
   /// </summary>
   /// <returns>The primary key of the entity.</returns>
   public Ulid GetPrimaryKey()
     => _pk;
+
+  /// <inheritdoc />
+  public sealed override string ToString()
+    => _pk.ToString();
+
+  /// <inheritdoc />
+  public override bool Equals(object? obj)
+    => obj is BaseEntity other && Equals(other);
+
+  /// <inheritdoc />
+  public override int GetHashCode()
+    => _pk.GetHashCode();
 }
